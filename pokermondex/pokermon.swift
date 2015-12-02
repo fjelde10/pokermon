6b31265570d4abefa7373a1a1cbaf3fbabe4cdb4
fjelde10 <brianjfjelde@hotmail.com>
2015-12-03 00:01:52 +0100

    Oploade new file for pokermon

---
+++ pokermondex/pokermon.swift
@@ -0,0 +1,30 @@
+//
+//  pokermon.swift
+//  pokermondex
+//
+//  Created by brian on 02/12/15.
+//  Copyright © 2015 bjfmedia. All rights reserved.
+//
+
+import Foundation
+
+class pokermon {
+
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    
+    }
+
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+     
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
